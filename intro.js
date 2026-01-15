ce1595a5beded8bbe7a2029b51c0bac178b0f2b8
mukumbwamwila03-byte <mukumbwamwila03@gmail.com>
2026-01-15 12:26:15 +0200

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,6 @@
+// intro.js - Personal Introduction
+console.log("=== Personal Introduction ===");
+console.log("Name: Mwira Mcdonald Mukumbwa"); 
+console.log("Age: 25"); 
+console.log("Favorite Programming Language: JavaScript"); 
+console.log("=============================");
